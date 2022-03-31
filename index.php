c327817d58f4975047c4dd32d9c2aa78660599f1
Realdens <denis.eleyehou@hotmail.fr>
2022-03-31 09:13:34 +0200

    Instanciation avec et sans constructeur / Héritage

--- index.php
+++ index.php
@@ -11,7 +11,62 @@
 
     <h2>Instanciation</h2>
     <?php
+        function my_var_dump($var) {
+            echo "<pre style='background-color: orange; border: 2px solid black'>";
+            var_dump($var);
+            echo "</pre>";
+        }
+
         include_once("user.class.php");
+        $user1 = new User("Dupond", "email1@mail.com", "DUP");
+        $user2 = new User("Durand", "email2@mail.com", "DUR");
+        $user3 = new User("Duboit", "email3@mail.com", "DUB");
+        $user4 = new User("1234", "1234@mail.com", "123");
+
+        //$user1->pseudo = "Dupont"; si la propriété pseudo était public
+        //$user1->setPseudo("Dupont"); //On passe par un setter car propriété privée
+        /*$user1->setEmail("dupont@gmail.com");
+        $user2->setPseudo("Durant");
+        $user3->setPseudo("Dubois");
+        $user4->setPseudo("123456789"); */
+
+        if ($user1->envoyerEmail("Le titre", "Le texte du mail")) {
+            echo "<p>Mail envoyé !</p>";
+        } else {
+            echo "<p>Mail non envoyé !</p>";
+        }
+
+        my_var_dump($user1);
+
+        $user1->bloquer();
+
+        my_var_dump($user1);
+        my_var_dump($user2);
+        my_var_dump($user3);
+        my_var_dump($user4);
+    
+    ?>
+    <h2>Héritage</h2>
+
+    <?php
+        include_once("admin.class.php");
+        $admin1 = new Admin("Admin1", "admin@mail.com", "ADM");
+        my_var_dump($admin1);
+    ?>
+</body>
+</html>
+
+
+ 
+       <?php
+/*
+        function my_var_dump($var) {
+            echo "<pre style='background-color: orange; border: 2px solid black'>";
+            var_dump($var);
+            echo "</pre>";
+        }
+       
+       include_once("user.class.php");
         $user1 = new User();
         $user2 = new User();
         $user3 = new User();
@@ -24,11 +79,17 @@
         $user3->setPseudo("Dubois");
         $user4->setPseudo("");
 
-        $user1->envoyerEmail("Le titre", "Le texte du mail");
-        
-        // var_dump($user4);
-    
+        if ($user1->envoyerEmail("Le titre", "Le texte du mail")) {
+            echo "<p>Mail envoyé !</p>";
+        } else {
+            echo "<p>Mail non envoyé !</p>";
+        }
+
+        my_var_dump($user1);
+
+        $user1->bloquer();
+
+        my_var_dump($user1);
+    */
     ?>
 
-</body>
-</html>
--- user.class.php
+++ user.class.php
@@ -9,6 +9,16 @@ class User
     private $actif;
 
     // Méthodes
+    public function __construct($pseudo, $email, $signature) {
+        //echo "Construct !";
+        $this->pseudo = $pseudo;
+        $this->email = $email;
+        $this->signature = $signature;
+        $this->actif = true;
+    }
+    public function __Destruct() {
+        echo "Destruct de " . $this->pseudo . "!";
+    }
     public function getPseudo() // fonction sans paramètres
     {
         return $this->pseudo;
@@ -30,6 +40,12 @@ class User
         }
     }
     public function envoyerEmail($titre, $message) {
-        mail($this->email, $titre, $message);
+        $retour = mail($this->email, $titre, $message);
+        //if (!$retour and DEBUG) my_log("Pb mail !");
+        return $retour;
+    }
+    public function bloquer() {
+        $this->actif = false;
+        $this->envoyerEmail("Alerte user !", "Vous êtes bloqué !");
     }
 }
